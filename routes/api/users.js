f43c12518d6d03a18bdf22078f94037c6c903889
Andrem <andreaif4495@gmail.com>
2019-04-21 00:16:40 -0400

    starting front end

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,7 +18,7 @@ const User = require('../../models/User');
 //@access Public
 router.get('/test', (req, res) => res.json({ msg: "Users Woks" }));
 
-//@route  GET api/user/register
+//@route  POST api/user/register
 //@desc   Register user
 //@access Public
 router.post('/register', (req, res) => {
