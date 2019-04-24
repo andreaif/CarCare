e98c14dae2a7be6d7ce1fe017dd7d86d424b075e
Nicholas Montanez <nicholasmontanez@Nicholass-MacBook-Pro.local>
2019-04-23 22:05:04 -0400

    group forms

--- client/src/components/create-carprofile/CreateCarProfile.js
+++ client/src/components/create-carprofile/CreateCarProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
 
 class CreateCarProfile extends Component {
   constructor(props) {
@@ -46,4 +49,4 @@ const mapStateToProps = state => ({
   errors: state.errors,
 })
 
-export default connect(null)(CreateCarProfile);
+export default connect(mapStateToProps)(CreateCarProfile);
